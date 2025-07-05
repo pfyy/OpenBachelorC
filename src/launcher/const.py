762f90631e217aa4418c818bce9dfd76d308bbc2
pfyy <112098515+pfyy@users.noreply.github.com>
2025-07-05 10:57:57 +0800

    fix package name

--- src/launcher/const.py
+++ src/launcher/const.py
@@ -3,4 +3,4 @@ from config import config
 if config["use_gadget"]:
     PACKAGE_NAME = "anime.pvz.online.en"
 else:
-    PACKAGE_NAME = "com.hypergryph.arknights"
+    PACKAGE_NAME = "com.YoStarEN.Arknights"
