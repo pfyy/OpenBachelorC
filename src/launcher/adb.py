66116d3b9bd1c70b78a3ba8bbd47c685857d57a3
pfyy <112098515+pfyy@users.noreply.github.com>
2025-07-06 00:43:38 +0800

    16.5.9

--- src/launcher/adb.py
+++ src/launcher/adb.py
@@ -16,11 +16,11 @@ MAX_NUM_MUMU_EMU = 4
 MAX_NUM_LD_EMU = 4
 
 ARCH_TO_FRIDA_SERVER_XZ_FILEPATH = {
-    "arm64-v8a": "frida-server/frida-server-16.6.6-android-arm64.xz",
-    "x86_64": "frida-server/frida-server-16.6.6-android-x86_64.xz",
+    "arm64-v8a": "frida-server/frida-server-16.5.9-android-arm64.xz",
+    "x86_64": "frida-server/frida-server-16.5.9-android-x86_64.xz",
 }
 
-ANDROID_FRIDA_SERVER_FILEPATH = "/data/local/tmp/florida-16.6.6"
+ANDROID_FRIDA_SERVER_FILEPATH = "/data/local/tmp/florida-16.5.9"
 
 TMP_DIRPATH = "tmp/"
 
