0fb610fc63fc132322c22c8166a5b6c26227b7cf
pfyy <112098515+pfyy@users.noreply.github.com>
2025-07-05 16:37:07 +0800

    1666 ver crypt

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     entry: {
-        "java": "./src/script/java/index.js",
-        "native": "./src/script/native/index.js",
-        "extra": "./src/script/extra/index.js",
-        "trainer": "./src/script/trainer/index.js",
+        "java": "./src/script1666/java/index.js",
+        "native": "./src/script1666/native/index.js",
+        "extra": "./src/script1666/extra/index.js",
+        "trainer": "./src/script1666/trainer/index.js",
     }
 };
