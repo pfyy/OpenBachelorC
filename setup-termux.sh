57434da9df22846957c98b85ddfc7b5de0258003
pfyy <112098515+pfyy@users.noreply.github.com>
2026-03-04 23:39:38 +0800

    termux

---
+++ setup-termux.sh
@@ -0,0 +1,17 @@
+pkg upgrade
+
+pkg install git
+pkg install python python-pip
+pkg install rust
+pkg install binutils
+
+export ANDROID_API_LEVEL=24
+
+pip install pipx
+pipx install poetry
+pipx ensurepath
+
+poetry config installer.max-workers 1
+
+poetry install || true
+poetry install --only-root
