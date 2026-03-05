c11cb01afdb56ee841f90e2a732d904ea9c63178
pfyy <112098515+pfyy@users.noreply.github.com>
2026-03-05 22:02:06 +0800

    version var

--- termux/build.sh
+++ termux/build.sh
@@ -2,20 +2,22 @@ set -euo pipefail
 
 pkg install -y xz-utils
 
-tar -xf frida-17.6.2.tar.gz
+FRIDA_VERSION=17.6.2
+
+tar -xf "frida-${FRIDA_VERSION}.tar.gz"
 
 mkdir -p devkit/
-tar -xf frida-core-devkit-17.6.2-android-arm64.tar.xz -C devkit/
+tar -xf "frida-core-devkit-${FRIDA_VERSION}-android-arm64.tar.xz" -C devkit/
 
 clang -shared -fPIC \
     -o _frida.so \
     -I devkit/ \
     $(python3-config --includes) \
-    frida-17.6.2/frida/_frida/extension.c \
+    "frida-${FRIDA_VERSION}/frida/_frida/extension.c" \
     devkit/libfrida-core.a \
     $(python3-config --ldflags) \
     -llog
 
 export FRIDA_EXTENSION=$(realpath _frida.so)
 
-poetry run pip install frida-17.6.2.tar.gz
+poetry run pip install "frida-${FRIDA_VERSION}.tar.gz"
