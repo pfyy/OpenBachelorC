ed9fdbe190d7ebfbb116b206cc89a8e992fe6d5d
pfyy <112098515+pfyy@users.noreply.github.com>
2026-07-04 22:15:17 +0800

    en

--- src/launcher/openbachelorc/const.py
+++ src/launcher/openbachelorc/const.py
@@ -2,7 +2,7 @@ from .config import config
 
 if config["use_gadget"]:
     PACKAGE_NAME = "anime.pvz.online"
-    # PACKAGE_NAME = "anime.pvz.online.en"
+    PACKAGE_NAME = "anime.pvz.online.en"
 else:
     PACKAGE_NAME = "com.hypergryph.arknights"
-    # PACKAGE_NAME = "com.YoStarEN.Arknights"
+    PACKAGE_NAME = "com.YoStarEN.Arknights"
